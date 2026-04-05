4719cc49d41cbc30915bf4e7477e46dfab6ed952
jumshud007 <qabulovjumshud@gmail.com>
2026-04-05 03:33:21 -0700

    dz1

---
+++ src/Firstjavacode/Hello.java
@@ -0,0 +1,17 @@
+package Firstjavacode;
+
+public class Hello {
+ public static void main(String[] args) {
+	System.out.println("Hello");
+	System.out.println(args.length); // 0
+}
+}
+//  TASK 2
+//  Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+//	Syntax error, insert "}" to complete ClassBody
+//	at Firstjavacode.Hello.main(Hello.java:6)
+
+
+//Шпаргалка
+//На одной странице выпиши руками или в файле cheatsheet.txt: две команды (javac / java), правило про имя класса при запуске, что делать, если «cannot find symbol».
+//javac-компилирует код java-запускает код
