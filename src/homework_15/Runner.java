af692db239bd440db3074f823b5e87c91e106842
jumshud007 <qabulovjumshud@gmail.com>
2026-04-20 10:37:43 -0700

    classwork_15

---
+++ src/homework_15/Runner.java
@@ -0,0 +1,40 @@
+package homework_15;
+
+public class Runner {
+	public static void main(String[] args) {
+		// 1
+		byte a = 25;
+		short b = a;
+		int c = b;
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		// 2
+		double d = 34.45;
+		float f = (float) d;
+
+		System.out.println(f);
+		// 3
+		long l = 260L;
+		byte m = (byte) l;
+
+		System.out.println(b);
+		// 4
+		int age = 20;// 20
+		float price = 15.5F;// 15.5F
+		char grade = 'A';// 'A'
+		String name = "Java";// "Java"
+		boolean isOpen = true;// true
+		// 5
+// 5	public void run() { }
+
+// 1	package lesson;
+
+// 2    import java.util.ArrayList;
+
+// 3    String name;
+
+// 4    public Runner() { }
+	}
+}
