da30b6f08812dc13c27af215ff87bf6dd5b9cfae
jumshud007 <qabulovjumshud@gmail.com>
2026-05-03 03:53:12 -0700

    homework_16

---
+++ src/homework_16/Runnerr.java
@@ -0,0 +1,86 @@
+package homework_16;
+
+public class Runnerr {
+ 	public static void main(String[] args) {
+    //5.1
+ 	// 1) 
+ 	//	asds -допустимо
+    //  b    -допустимо
+ 	//  $    -допустимо
+ 	//  _    -допустимо
+ 	
+ 	// !     -недопустимо нельзя использовать символы
+ 	// +     -недопустимо нельзя использовать символы
+ 	// 1     -недопустимо нельзя использовать цифры
+ 	// /     -недопустимо нельзя использовать символы
+ 		
+ 	// 2)
+ 	//- int int = 5; // VariableDeclaratorId
+ 	//+ int int1 = 5;
+ 		
+ 	// 3)	
+    int Value = 1;
+    int value = 2;
+    // Java ucitivaet registr
+    
+    // 4)
+    int user_name = 12;
+    int maxScore = 13;
+    System.out.println(user_name + "," + maxScore);
+    
+    // 5)
+    int x= 1;
+    System.out.println(x);
+    sth();
+ 	}
+	static int x = 2;
+ 	public static void sth(){ 
+        System.out.println(x);
+ 	}// local peremennuyu esli vizvat tam qde ona naxoditsa toqda pokajut ee znaceniye a druqaya peremennaya global
+
+ 	// 5.2
+ 	// 1)
+ 	// if,int,double,float,integer,try,public,private,class,return
+ 	// 2)
+ 	// boolean return = true - boolean returnnr = true +
+ 	// main - eto tipo qlavaniy metod
+ 	// 3)
+ 	//var s = "ok"; var n = 5;
+ 	// ключевое   литерал   нет
+ 	// try        124        !
+ 	// int         2        0832
+ 	// try        124        !
+ 	// try        124        !
+ 	// try        124        !
+ 	// try        124        !
+ 	// if          "fsd"        !
+ 	// try        124        !
+ 	// 5.3
+ 	//1)
+ 	// max,min,amount
+ 	//2)
+ 	boolean isLoggedIn = true;
+ 	boolean hasPermission = false;// prefiksi razreseni java potomu cto registi razreseni
+ 	//3)
+ 	//Напишите одну строку с total_price (если компилятор позволит) и перепишите на totalPrice — что читается легче для Java-разработчика?
+ 	//второй
+ 	public static final double PI_APPROX = 3.14159;
+ 	public void sth1() {
+ 		System.out.println(PI_APPROX);
+ 	}
+ 	//Таблица: элемент (класс, метод, константа, пакет) | пример правильного имени | правило одной строкой.
+ 	//1)Runner - classi s bolsoy bukvi
+ 	//2)count
+ 	//3)money
+ 	//4)homewoork_16- packagu s malenkoy bukvi 
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+}
